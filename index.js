d435d743bb8452830bacd2ebc6f6ca6f234f5068
Nishant Sharma <apsanishat@gmail.com>
2023-03-02 18:31:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-function shadowizard(options) {
-    let images = document.querySelectorAll('.shadowizard');
+import { shadowizard } from '@developerso/shadowizard';
 
-    if(options.shadow_type == 'hard'){
-        options.shadow_type = '0px'
-    }else{
-        options.shadow_type = '15px'
-    }
-    images.forEach(image =>{
-        image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
-        if(options.padding){
-            image.style.padding - '1em'
-        }
-    })
-}
-module.exports.shadowizard = shadowizard
+shadowizard({
+  shadow_type: 'soft',
+  padding: true,
+  border: true,
+  border_radius: true,
+  width: '200px',
+  height: '200px',
+  background_color: '#ffffff',
+  gradient: true,
+  gradient_direction: 'to right',
+  gradient_color: '#000000'
+});
