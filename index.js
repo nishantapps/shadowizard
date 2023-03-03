3bce1dc5ffef4582c6f1530d9eee687de316d800
Nishant Sharma <apsanishat@gmail.com>
2023-03-03 22:14:58 +0530

    Added Animation
    Added animation on mouse hover

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ function shadowizard(options) {
     let images = document.querySelectorAll('.shadowizard');
 
     if(options.shadow_type == 'hard'){
-        options.shadow_type = '0px'
-    }else{
-        options.shadow_type = '15px'
+        options.shadow_type = '0px';
+    } else {
+        options.shadow_type = '15px';
     }
 
     images.forEach(image =>{
         image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
         if(options.padding){
-            image.style.padding = '1em'; // Added missing '=' operator
+            image.style.padding = '1em';
         }
         if(options.border){
             image.style.border = '1px solid black';
@@ -31,7 +31,19 @@ function shadowizard(options) {
                 image.style.backgroundColor = options.background_color;
             }
         }
-    })
+        
+        if(options.animation){ // check if animation property is set
+            image.style.transition = 'all 0.5s ease-in-out'; // add transition effect
+            image.addEventListener('mouseover', () => { // add mouseover event listener to trigger animation
+                image.style.transform = 'translateY(-5px)'; // move image up
+                image.style.boxShadow = `10px 15px ${options.shadow_type} 1px rgba(0,0,0,0.24)`; // change box shadow
+            });
+            image.addEventListener('mouseleave', () => { // add mouseleave event listener to reset animation
+                image.style.transform = 'translateY(0px)'; // move image down
+                image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`; // reset box shadow
+            });
+        }
+    });
 }
 
 module.exports.shadowizard = shadowizard;
